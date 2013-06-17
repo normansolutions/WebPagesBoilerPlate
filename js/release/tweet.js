ae81c1a43c38f9361d533c21f0eef64302068fff
normansolutions <hi@normansolutions.co.uk>
2013-06-17 15:59:52 +0100

    Added Twitter integration with TweetSharp

---
+++ js/release/tweet.js
@@ -0,0 +1,31 @@
+﻿function InOut(elem) {
+    elem.delay()
+        .fadeIn()
+        .delay(4000)
+        .fadeOut(
+
+    function () {
+        if (elem.next().length > 0) // if there is a next element
+        {
+            InOut(elem.next());
+        } // use it
+        else {
+            InOut(elem.siblings(':first'));
+        } // if not, then use go back to the first sibling
+
+    });
+}
+
+(function () {
+    var url = '\methods/Tweet';
+    $.when($.getJSON(url, function (data) {
+        $.each(data, function (i, item) {
+            var json = $.parseJSON(item.RawSource)
+            $('#tweets').append('<li>' + $.timeago(json.created_at) + ': ' + item.TextAsHtml + '</li>');
+        });
+    })).then(function () {
+        var tweets = $('#tweets');
+        tweets.find('li').hide();
+        InOut(tweets.find('li:first'));
+    });
+})();
