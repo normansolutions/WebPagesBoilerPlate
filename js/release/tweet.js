944e66b67026eb14ab0bea1595de02ce3653e3d9
normansolutions <hi@normansolutions.co.uk>
2013-06-18 15:42:11 +0100

    Fix timeago date format for ipad etc

--- js/release/tweet.js
+++ js/release/tweet.js
@@ -16,12 +16,18 @@
     });
 }
 
+function parseDate(str) {
+    var v = str.split(' ');
+    return new Date(Date.parse(v[1] + " " + v[2] + ", " + v[5] + " " + v[3] + " UTC"));
+}
+
+
 (function () {
     var url = '\methods/Tweet';
     $.when($.getJSON(url, function (data) {
         $.each(data, function (i, item) {
             var json = $.parseJSON(item.RawSource)
-            $('#tweets').append('<li>' + $.timeago(new Date(Date.parse(json.created_at.replace(/( \+)/, ' UTC$1')))) + ': ' + item.TextAsHtml + '</li>');
+            $('#tweets').append('<li>' + $.timeago(parseDate(json.created_at)) + ': ' + item.TextAsHtml + '</li>');
         });
     })).then(function () {
         var tweets = $('#tweets');
