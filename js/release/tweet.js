5e8f51b7ba91773c5a0914d195925c4c6d7b8dda
normansolutions <hi@normansolutions.co.uk>
2013-09-20 11:50:50 +0100

    Made Twitter Feed Hyperlink

--- js/release/tweet.js
+++ js/release/tweet.js
@@ -22,12 +22,46 @@ function parseDate(str) {
 }
 
 
+function twitterLinks(text) {
+    var base_url = 'http://twitter.com/';   // identica: 'http://identi.ca/'
+    var hashtag_part = 'search?q=#';        // identica: 'tag/'
+    // convert URLs into links
+    text = text.replace(
+        /(>|<a[^<>]+href=['"])?(https?:\/\/([-a-z0-9]+\.)+[a-z]{2,5}(\/[-a-z0-9!#()\/?&.,]*[^ !#?().,])?)/gi,
+        function ($0, $1, $2) {
+            return ($1 ? $0 : '<a href="' + $2 + '" target="_blank">' + $2 + '</a>');
+        });
+    // convert protocol-less URLs into links        
+    text = text.replace(
+        /(:\/\/|>)?\b(([-a-z0-9]+\.)+[a-z]{2,5}(\/[-a-z0-9!#()\/?&.]*[^ !#?().,])?)/gi,
+        function ($0, $1, $2) {
+            return ($1 ? $0 : '<a href="http://' + $2 + '">' + $2 + '</a>');
+        });
+    // convert @mentions into follow links
+    text = text.replace(
+        /(:\/\/|>)?(@([_a-z0-9\-]+))/gi,
+        function ($0, $1, $2, $3) {
+            return ($1 ? $0 : '<a href="' + base_url + $3
+                + '" title="Follow ' + $3 + '" target="_blank">@' + $3
+                + '</a>');
+        });
+    // convert #hashtags into tag search links
+    text = text.replace(
+        /(:\/\/[^ <]*|>)?(\#([_a-z0-9\-]+))/gi,
+        function ($0, $1, $2, $3) {
+            return ($1 ? $0 : '<a href="' + base_url + hashtag_part + $3
+                + '" title="Search tag: ' + $3 + '" target="_blank">#' + $3
+                + '</a>');
+        });
+    return text;
+}
+
 (function () {
     var url = '\methods/Tweet';
     $.when($.getJSON(url, function (data) {
         $.each(data, function (i, item) {
             var json = $.parseJSON(item.RawSource)
-            $('#tweets').append('<li>' + $.timeago(parseDate(json.created_at)) + ': ' + item.TextAsHtml + '</li>');
+            $('#tweets').append('<li>' + twitterLinks($.timeago(parseDate(json.created_at)) + ': ' + item.Text) + '</li>');
         });
     })).then(function () {
         var tweets = $('#tweets');
